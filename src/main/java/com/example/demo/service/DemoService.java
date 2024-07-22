35825fcc30b3754ffbc86ea6d5c6b3a6396a521b
thomas-nuessler <thomas.nuessler@gmx.de>
2024-07-22 22:55:54 +0200

    Logging

--- src/main/java/com/example/demo/service/DemoService.java
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -4,6 +4,9 @@ import com.example.demo.model.City;
 import com.example.demo.model.Weather;
 import com.example.demo.repository.CityRepository;
 import com.example.demo.repository.WeatherRepository;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,31 +18,28 @@ import java.util.List;
 @Service
 public class DemoService {
 
+	private static final Logger logger = LogManager.getLogger(DemoService.class);
     @Autowired
     private WeatherRepository weatherRepository;
 
     @Autowired
     private CityRepository cityRepository;
 
-    //@GetMapping("/weathers")   // GET Method for reading operation
     public List<Weather> listWeather(String city, Integer page, Integer size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by("date").descending());
 
         return weatherRepository.listWeather(city, pageable);
     }
 
-    //@PutMapping("/weather/{weather}")    // PUT Method for Update operation
     public Weather saveWeather(Weather weather) {
         return weatherRepository.save(weather);
     }
 
-    //@GetMapping("/weather")   // GET Method for reading operation
     public Weather getWeather(Long id) {
         return weatherRepository.findById(id).orElse(null);
     }
 
-    //@DeleteMapping("/weather/{id}")    // DELETE Method for Delete operation
-    public Boolean deleteWeather(Long id) {
+     public Boolean deleteWeather(Long id) {
         Weather weather = weatherRepository.findById(id).orElse(null);
         if (weather == null) {
             return true;
@@ -50,12 +50,10 @@ public class DemoService {
         return true;
     }
 
-    //@GetMapping("/city")   // GET Method for reading operation
-    public City getCity(String name) {
+     public City getCity(String name) {
         return cityRepository.findByName(name);
     }
 
-    //@PutMapping("/city/{id}")    // PUT Method for Update operation
     public City saveCity(City city) {
         return cityRepository.save(city);
     }
